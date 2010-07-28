b64b09b3787d12d50ba4b8d72f7f3ac62028c1bd
joshua.short <joshua.short@3bada60e-6451-b4c1-cebc-71c999ccb1ba>
2010-07-28 08:56:40 +0000

    Adding comments to the source file to assist understanding. Added README with some simple usage cases.

--- sqlitefs.py
+++ sqlitefs.py
@@ -4,39 +4,59 @@
 # Author: Joshua Short <joshua.short@gmail.com>
 # Description: Class for File System Access to a Sqlite Database
 
-# The structure, or entities, of a a sqlite database:
-STRUCTURE = ('table', 'index', 'trigger')
+# there are three primary structures in a sqlite database:
+STRUCTURE = [
+  ('table', "tables"),
+  ('index', "indexes"),
+  ('trigger', "triggers"),
+]
 
-# helper method for representing a database's schema as a dictionary
-# TODO: implementing caching which auto-updates when structure changes
+# TODO: implement auto-updating cache
+# helper method to represent the structure of a sqlite database as a dictionary
 def get_structure(db):
   return dict([
-    (type, db.execute("SELECT * FROM sqlite_master WHERE type=?", (type,)).fetchall())
-    for type
+    (display, db.execute("SELECT * FROM sqlite_master WHERE type=?", (type,)).fetchall())
+    for type, display
     in STRUCTURE
   ])
 
-
-import sqlite3
+# XXX: This will be a show stopper until I upload the required modules
+# We can load code directly from source files, treating dictionary as an environment
 import system
 Filesystem = system.Code("/System/Storage.lib/filesystem.py")['Filesystem']
+
+###############################################################################
+# The SqliteFilesystem Class determines how databases are represented.
+###############################################################################
+import sqlite3
 class SqliteFilesystem(Filesystem):
   
+	# To create a File System, give the location of a sqlite database
+	# If location is omitted (or None), the database will reside in memory
 	def __init__(self, location=None):
 	    Filesystem.__init__(self)
 	    self.db = sqlite3.connect(location or ":memory:", check_same_thread=False)
-		
+	    
+	# __getitem__ retrieves items from the file system which match the requested path
 	def __getitem__(self, path, fh=None):
+	  
+	    # The Filesystem class provides a simple query processing method: splitting on '/'
 	    query = self.process_query(path)
+	    
+	    # Instead of working with many errors, use this set as a visual error flag
 	    response = ["XXX"]
 
-	    # /
+	    # If the root directory was requested, 
 	    if path == "/":
-		response = [item for item in STRUCTURE]
+	      
+		# The response is the display names for the elemental structures
+		response = [display for type, display in STRUCTURE]
 		
-	    elif query[1] not in STRUCTURE:
+	    # This checks to see if the query is not one of the three allowed values ('tables', 'indexes', 'triggers')
+	    elif len([display for type, display in STRUCTURE if display == query[1]]) == 0:
 		response = None
 
+	    # The query appears to be vaild, so it gets passed along for further analysis
 	    else:
 		if len(query) == 2:
 			response = [str(name) for type, name, target, id, string in get_structure(self.db)[query[1]]]
